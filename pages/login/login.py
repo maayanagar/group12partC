8225f9ae3de609e8f6c1937b5280fc622d63e3ea
Barak Pinchovski <barakpinch@gmail.com>
2020-03-11 13:10:52 +0200

    Code cleanup

--- pages/login/login.py
+++ pages/login/login.py
@@ -2,12 +2,14 @@ from flask import Blueprint, render_template, request, session
 from consts.consts import methods
 
 
+# login blueprint definition
 login = Blueprint('login', __name__, static_folder='static', static_url_path='/login', template_folder='templates')
 
 
+# Routes
 @login.route('/login', methods=['GET', 'POST'])
 def index():
-    if request.method == methods['post']:
+    if request.method == methods['post'] and 'username' not in session:
         session['username'] = request.form['username']
         return render_template('logged_in.html', username=session.get('username'))
     elif request.method == methods['get']:
--- pages/logout/logout.py
+++ pages/logout/logout.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 
 
+# logout blueprint definition
 logout = Blueprint('logout', __name__, static_folder='static', static_url_path='/logout', template_folder='templates')
 
 
+# Routes
 @logout.route('/logout')
 def index():
     if 'username' in session:

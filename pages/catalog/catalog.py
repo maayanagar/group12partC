f458e7b4351cb71d7607fe8a40e05aea3b3564cc
Barak Pinchovski <barakpinch@gmail.com>
2020-05-17 00:31:48 +0300

    Project cleanup

--- pages/catalog/catalog.py
+++ pages/catalog/catalog.py
@@ -1,18 +1,10 @@
 from flask import Blueprint, render_template
 
-
+# catalog blueprint definition
 catalog = Blueprint('catalog', __name__, static_folder='static', static_url_path='/catalog', template_folder='templates')
 
 
+# Routes
 @catalog.route('/catalog')
 def index():
     return render_template('catalog.html')
-    #return 'hi catalog'
-
-# @catalog.route('/', defaults={ 'page': 'index' })
-# @catalog.route('/<page>')
-# def show(page):
-#     try:
-#         return render_template('catalog.html')
-#     except TemplateNotFound:
-#         abort(404)

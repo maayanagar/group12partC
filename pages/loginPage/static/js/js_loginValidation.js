896e4ee7618c87832526b37cf425022af2d60535
noashush <noashush@post.bgu.ac.il>
2024-03-30 13:50:39 +0300

    bla

--- pages/loginPage/static/js/js_loginValidation.js
+++ pages/loginPage/static/js/js_loginValidation.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const userData = {
             username: document.getElementById("username").value,
             password: document.getElementById("password").value
+
         }
        /* const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
